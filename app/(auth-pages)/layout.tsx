4dd5cb3ea00fe17cf5811506ba384655c2513c99
AtanasovCode <a_ljupce@yahoo.com>
2025-03-22 20:14:36 +0100

    add styling for layout auth pages

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -4,6 +4,11 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="max-w-7xl flex flex-col gap-12 items-start">{children}</div>
+    <div className="min-h-screen w-full flex flex-col justify-start md:flex-row md:justify-center">
+      <div className="w-full h-1/4 md:min-w-[70%]"></div>
+      <div className="h-screen w-full flex flex-1 items-center justify-center gap-12 p-4">
+        {children}
+      </div>
+    </div>
   );
 }
--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -20,14 +20,8 @@ export default async function Signup(props: {
 
   return (
     <>
-      <form className="flex flex-col min-w-64 max-w-64 mx-auto">
-        <h1 className="text-2xl font-medium">Sign up</h1>
-        <p className="text-sm text text-foreground">
-          Already have an account?{" "}
-          <Link className="text-primary font-medium underline" href="/sign-in">
-            Sign in
-          </Link>
-        </p>
+      <form className="w-full flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-medium">Create a new account</h1>
         <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
           <Label htmlFor="email">Email</Label>
           <Input name="email" placeholder="you@example.com" required />
@@ -44,8 +38,13 @@ export default async function Signup(props: {
           </SubmitButton>
           <FormMessage message={searchParams} />
         </div>
+        <p className="text-sm text text-foreground">
+          Already have an account?{" "}
+          <Link className="text-primary font-medium underline" href="/sign-in">
+            Sign in
+          </Link>
+        </p>
       </form>
-      <SmtpMessage />
     </>
   );
 }
