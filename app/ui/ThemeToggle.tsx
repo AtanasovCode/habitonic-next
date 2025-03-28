a563c8116229ff53b8227aaf659a82178d66534a
AtanasovCode <a_ljupce@yahoo.com>
2025-03-29 00:53:44 +0100

    add theme toggle

---
+++ app/ui/ThemeToggle.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function ThemeToggle() {
+
+    const { setTheme } = useTheme();
+
+    const changeTheme = (theme: string) => {
+        setTheme(theme);
+    }
+
+    return (
+        <div className="flex items-center justify-start">
+            <div className="p-3 bg-accent cursor-pointer" onClick={() => changeTheme("system")}>System</div>
+            <div className="p-3 bg-accent cursor-pointer"  onClick={() => changeTheme("dark")}>Dark</div>
+            <div className="p-3 bg-accent cursor-pointer"  onClick={() => changeTheme("light")}>Light</div>
+        </div>
+    );
+}
