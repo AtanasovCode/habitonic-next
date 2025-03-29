d2c8859416aa87ebceeec693c19d8e12997103ac
AtanasovCode <a_ljupce@yahoo.com>
2025-03-29 18:11:43 +0100

    created mobile styling for HeroNav component

--- app/ui/home/HeroNav.tsx
+++ app/ui/home/HeroNav.tsx
@@ -1,10 +1,29 @@
+'use client';
+
+import { useHabitStore } from "@/useHabitStore";
+import clsx from "clsx";
+
 export default function HeroNav() {
+
+    const { showMobileNav, toggleMobileNav } = useHabitStore();
+
     return (
-        <nav className="w-full flex items-center justify-between px-6 py-4 bg-primary text-text">
+        <nav className="w-full flex items-center justify-center lg:justify-between px-6 py-4 bg-primary text-text">
             <div className="font-bold text-2xl">
                 Habitonic
             </div>
-            <div className="flex items-center justify-center gap-8 text-lg">
+            <div 
+                className={clsx(
+                    "w-full h-dvh bg-accent flex flex-col items-start justify-start fixed top-0 z-50 p-6 gap-6 text-md transition-transform duration-300 ease-in-out lg:w-auto lg:h-auto lg:flex-row lg:items-center lg:justify-center lg:gap-8 lg:text-lg",
+                    {
+                        "-translate-x-[200%] lg:translate-x-0": !showMobileNav,
+                        "translate-x-0 lg:translate-x-0": showMobileNav
+                    }
+                )}
+            >
+                <div className="text-bold text-2xl w-full flex items-center justify-center lg:hidden">
+                    Habitonic
+                </div>
                 <div>Create Account</div>
                 <div>Log In</div>
                 <div>Theme</div>
